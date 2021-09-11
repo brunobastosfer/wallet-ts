6127c764d42dc49c1f8235148e89c67a47b97d5e
brunobastosfer <brunobastos@edu.unifor.br>
2021-09-11 04:55:14 -0300

    iniciando o projeto, fazendo as configurações no css e instalando as dependências

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+// import styled from 'styled-components'
+import { GlobalStyle } from './styles/global';
+
+export function App() {
+  return (
+    <div className="App">
+     <h1>Hello word</h1>
+     <GlobalStyle />
+    </div>
+  );
+}
